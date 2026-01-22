4960943f25db0b5c872cc7696b8b59fe676066a0
Kaloyan19 <teodosievkaloyan@gmail.com>
2026-01-22 04:41:52 +0200

    Added custom user model with migrations

--- poker_project/settings.py
+++ poker_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tournaments',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'poker_project.urls'
+
+AUTH_USER_MODEL = 'tournaments.PokerUser'
 
 TEMPLATES = [
     {
